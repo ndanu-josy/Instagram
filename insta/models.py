5f3bcfc2b7fd2452f2fd1c1270fefcf38f5b3a57
ndanu-josy <josphinendanu3@gmail.com>
2021-07-10 16:08:10 +0300

    Add image model and methods

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.db.models.fields.related import ForeignKey
+from django.utils.html import linebreaks
 # Create your models here.
 
 class Profile(models.Model):
@@ -26,5 +28,27 @@ class Profile(models.Model):
     def update_profile(cls, id, profile):
         cls.objects.filter(id=id).update(profile=profile)
 
-       
+class Image(models.Model):
+    image = CloudinaryField('posts')
+    image_name= models.CharField(max_length=50, blank=True)
+    image_caption= models.CharField(max_length=400)
+    likes = models.ManyToManyField(User, related_name='likes', blank=True)
+    comments = models.CharField(max_length=30,blank=True)
+    profile = models.ForeignKey(Profile,on_delete = models.CASCADE)  
     
+    def __str__(self):
+        return self.image_name
+
+    def save_image(self):
+        self.save()
+
+    def delete_image(self):
+        self.delete()   
+
+    @property
+    def comments(self):
+        return self.comments.all()
+
+    def likes(self):
+        return self.likes.count()
+              
