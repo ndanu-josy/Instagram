827571c7ec4a098c991bee8c6fdf07cdd570a08d
ndanu-josy <josphinendanu3@gmail.com>
2021-07-14 11:51:57 +0300

    Update installed apps

--- instagram/settings.py
+++ instagram/settings.py
@@ -79,7 +79,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'doubletap.apps.DoubletapConfig',
+    'doubletap.apps.InstaConfig',
     'bootstrap3',
     'cloudinary',
 ]
